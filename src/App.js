03a908c2c513a96ab408c68b4304d8da594628d1
Belchenkov <u608110@gmail.com>
2017-09-08 15:43:22 +0300

    Core Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 // import $ from 'jquery';
 import './App.css';
+import  Range from './Components/Range';
+import Output from './Components/Output';
 
 class App extends Component {
 
   constructor(props) {
       super(props);
       this.state = {
-
+        height: 170,
+        weight: 65,
+        bmi: 22.49,
+        bmiClass: 'Normal'
       };
-
   }
-
   render() {
-
     return (
       <div className="App">
-        <h1>Calculator</h1>
+          <h1>BMI Calculator</h1>
+          <form>
+              <div>
+                  <label>Height</label>
+                  <Range />
+              </div>
+              <div>
+                  <label>Weight</label>
+              </div>
+          </form>
+          <br/><br/>
+          <Output />
       </div>
     );
   }
---
+++ src/Components/Output.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import $ from 'jquery';
+
+class Output extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h1>Output</h1>
+            </div>
+        );
+    }
+}
+
+export default Output;
